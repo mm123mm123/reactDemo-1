72a59aacb5fa53db32796715487c0e9394beb4b3
mimi <fiymi666@gmail.com>
2020-12-14 22:08:26 +0800

    安装svg-loader并插入图片

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-
+require('icons/money.svg')
+require('icons/labels.svg')
+require('icons/statistics.svg')
 const NavWrapper = styled.nav`
       box-shadow:0 0 3px rgba(0,0,0,0.25);
       ul{
@@ -18,12 +20,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg class-name='navIcon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg >
+            <use xlinkHref='#labels'/>
+          </svg>
           <Link to="/money">记一笔</Link>
         </li>
         <li>
+          <svg >
+            <use xlinkHref='#statistics'/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
