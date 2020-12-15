bbedd4dd7e550ec83d0c2e86790a5f207ee432d9
mimi <fiymi666@gmail.com>
2020-12-15 10:26:35 +0800

    配置导航图片样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-require('icons/money.svg')
-require('icons/labels.svg')
-require('icons/statistics.svg')
+
+require('icons/money.svg');
+require('icons/labels.svg');
+require('icons/statistics.svg');
 const NavWrapper = styled.nav`
       box-shadow:0 0 3px rgba(0,0,0,0.25);
       ul{
         display: flex;
         li{
-        text-align: center;
         width:33.3333%;
-        padding:16px;
+        padding:2px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        svg{
+          height:2em;
+          width:2em;
+        }
         }
       }
 `;
@@ -20,19 +27,19 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg class-name='navIcon'>
-            <use xlinkHref='#money'/>
+          <svg>
+            <use xlinkHref='#labels'/>
           </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
           <svg >
-            <use xlinkHref='#labels'/>
+            <use xlinkHref='#money'/>
           </svg>
           <Link to="/money">记一笔</Link>
         </li>
         <li>
-          <svg >
+          <svg>
             <use xlinkHref='#statistics'/>
           </svg>
           <Link to="/statistics">统计</Link>
