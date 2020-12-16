017ff986273946a836620ad5a4ec6d3cd5199794
mimi <fiymi666@gmail.com>
2020-12-16 18:57:17 +0800

    为标签组件添加功能

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection=styled.section`
+const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -9,6 +10,10 @@ const TagsSection=styled.section`
        background: #D9D9D9; border-radius: 18px;
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
+       &.selected{
+        background:#666;
+        color:#D9D9D9;
+       }
     }
   }
   > button{
@@ -16,5 +21,36 @@ const TagsSection=styled.section`
     border-bottom: 1px solid #333; color: #666;
     margin-top: 8px;
   }
-`
-export {TagsSection}
+`;
+const TagsSection: React.FC = () => {
+  const [tags,setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  function onToggleTag(tag: string) {
+    if (selectedTags.indexOf(tag) >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  }
+  function addTag(){
+    const tagName=window.prompt('请输入标签名')
+    if(tagName){
+      setTags([...tags,tagName])
+    }
+  }
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag => (
+          <li key={tag} onClick={() => onToggleTag(tag)}
+              className={selectedTags.indexOf(tag) >= 0 ? 'selected' : ''}>
+            {tag}
+          </li>
+        ))}
+      </ol>
+      <button onClick={addTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+export {TagsSection};
