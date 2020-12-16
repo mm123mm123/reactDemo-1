8868eaa8e26461e62e9610034b26473f0f4e3527
mimi <fiymi666@gmail.com>
2020-12-16 18:57:44 +0800

    为备注组件添加功能

--- src/views/money/NotesSection.tsx
+++ src/views/money/NotesSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NotesSection=styled.section`
+const Wrapper=styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
@@ -17,4 +18,25 @@ const NotesSection=styled.section`
     }
   }
 `
+const NotesSection: React.FC=()=>{
+  const [note,setNote]=useState('')
+  const refInput=React.useRef<HTMLInputElement>(null);
+  function updateNote(){
+    if(refInput.current!==null && refInput.current.value!==''){
+      setNote(refInput.current.value)
+    }
+  }
+  return(
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注"
+               ref={refInput}
+               onBlur={updateNote}
+               defaultValue={note}
+        />
+      </label>
+    </Wrapper>
+  )
+}
 export {NotesSection}
