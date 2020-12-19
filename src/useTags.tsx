8507d6aaa37b6aa976f84f35eb340d6b8d579e52
mimi <fiymi666@gmail.com>
2020-12-19 16:48:32 +0800

    使用js自带函数完成更新和删除标签功能

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -7,30 +7,28 @@ const defaultTags = [
   {id: createId(), name: '住'},
   {id: createId(), name: '行'},
 ];
+type tag = {
+  id: number, name: string
+}
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+  const [tags, setTags] = useState<tag[]>(defaultTags);
   const findTag = (id: string) => {
     return tags.filter(tag => tag.id === parseInt(id))[0];
   };
-  const findIndex=(id: string)=>{
-    return tags.map(tag => tag.id).indexOf(findTag(id).id);
-  }
-  const cloneTags=()=>{
-    return JSON.parse(JSON.stringify(tags))
-  }
   const updateTag = (id: string, obj: { name: string }) => {
-    const clonedTags = cloneTags();
-    const index = findIndex(id)
-    clonedTags.splice(index, 1, {id: parseInt(id), name: obj.name});
-    setTags(clonedTags);
+    const newTags = tags.map(tag => {
+      if (tag.id === parseInt(id)) {
+        return {id:parseInt(id),name:obj.name};
+      } else {
+        return tag;
+      }
+    });
+    setTags(newTags);
   };
-  const deleteTag=(id: string)=>{
-    const clonedTags = cloneTags();
-    const index = findIndex(id)
-    clonedTags.splice(index, 1);
-    setTags(clonedTags);
-  }
-  return {tags, setTags, findTag, updateTag,deleteTag};
+  const deleteTag = (id: string) => {
+    setTags(tags.filter(tag=>tag.id!==parseInt(id)));
+  };
+  return {tags, setTags, findTag, updateTag, deleteTag};
 };
 
 export {useTags};
